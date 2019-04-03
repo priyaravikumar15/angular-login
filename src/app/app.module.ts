38c6563b560a0d19bbfd049ac715218bfcfd494f
priyaravikumar15 <priyaravikumar15@gmail.com>
2019-04-03 15:29:08 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,7 +11,7 @@ import {SLoginComponent} from './login/slogin.component';
 import { routing } from './app.route';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,routing ],
+  imports:      [ BrowserModule, FormsModule,routing,NgbModule ],
   declarations: [ AppComponent, HelloComponent,LoginComponent,RegisterComponent,SLoginComponent ],
   bootstrap:    [ AppComponent ]
 })
